51173bdd9c3d84834ee16e625dc7d0a1d5b92495
landandair <jonhuwaldt@gmail.com>
2023-02-22 23:44:04 -0600

    Add Basic Drawing

--- Sim_Code/Airplane_Code.py
+++ Sim_Code/Airplane_Code.py
@@ -1,5 +1,20 @@
 """Holds Code for dealing with the plane vector and converting between the reference and aircraft frame of reference"""
+from pygame import sprite
+import pygame as pg
 
-class FakePlane:
-    def __init__(self):
-        self.ret = []
+class FakePlane(sprite.Sprite):
+    def __init__(self, start_pos):
+        super().__init__()
+        self.image = pg.image.load('Assets/Plane.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = start_pos
+
+    def update(self):
+        self.do_physics()
+        self.update_pos()
+
+    def do_physics(self):
+        pass
+
+    def update_pos(self):
+        pass
--- Sim_Code/Sim_Window.py
+++ Sim_Code/Sim_Window.py
@@ -1,5 +1,6 @@
 """Holds Code that handles the content of the window as well as containing all of the pygame sprite groups needed
 for the sim"""
+from Airplane_Code import FakePlane
 import pygame as pg
 import Pygame_Tools as Tool
 import sys
@@ -13,20 +14,29 @@ class MainWindow:
         self.h_w = 9/16  # height to width Ratio
         pg.display.init()
         self.monitor_size = np.array((pg.display.Info().current_w, pg.display.Info().current_h))
+
         if self.monitor_size[0] * self.h_w > self.monitor_size[1]:
             self.w = self.monitor_size[1]/self.h_w
         else:
             self.w = self.monitor_size[0]
-        self.screen = pg.display.set_mode([0,0], pg.FULLSCREEN)
+        self.h = self.w * self.h_w
+
+        self.screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+        # Turns 0-1 cords into pixel cords
+        self.f_real_xy = lambda xy: (xy[0]*self.w + (self.monitor_size[0]-self.w),
+                                     xy[1]*self.h + (self.monitor_size[1]-self.h))
         # Sprite Groups
         self.background = pg.sprite.Group()
-        back = Tool.Background(pg.image.load('Assets/Background1.png'), self.w,
-                               self.h_w*self.w)
+        back = Tool.Background(pg.image.load('Assets/Background1.png'), self.w, self.h)
         back.rect.center = self.monitor_size/2
         self.background.add(back)
 
+        self.planes = pg.sprite.Group()
+        plane = FakePlane(self.f_real_xy((.1,.1)))
+        self.planes.add(plane)
         # Misc Vars
         self.clock = pg.time.Clock()
+        self.pause = False
         self.quit = False
 
     def update(self):
@@ -34,7 +44,11 @@ class MainWindow:
         if self.quit:
             return
 
+        if not self.pause:
+            self.planes.update()
+
         self.background.draw(self.screen)
+        self.planes.draw(self.screen)
         pg.display.flip()
         self.clock.tick(60)
 
@@ -50,6 +64,8 @@ class MainWindow:
                     self.quit = True
                     pg.quit()
                     sys.exit()
+                if event.key == pg.K_p:
+                    self.pause = not self.pause
 
 
 
